0439cda744f8e490a144d01dd326c416ea6dfeac
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-05 11:48:58 +0100

    feat: counter working well

--- CounterApp/app/src/main/java/roberherraiz/counter/MainActivity.java
+++ CounterApp/app/src/main/java/roberherraiz/counter/MainActivity.java
@@ -2,12 +2,45 @@ package roberherraiz.counter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    public int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        count = 0;
+        showCounter();
     }
+
+    /* Las funciones de lógica de eventos deben recibir el View, osea, el botón en estos casos */
+
+    public void increaseCounter(View view) {
+        count++;
+        showCounter();
+    }
+
+    public void decreaseCounter(View view) {
+        count--;
+        showCounter();
+    }
+
+    public void resetCounter(View view) {
+        count = 0;
+        showCounter();
+    }
+
+    public void showCounter() {
+
+        /* Usamos un casting a textview del id de la view */
+        TextView counterText = findViewById(R.id.counterTextView);
+        counterText.setText("" + count);
+
+    }
+
 }
