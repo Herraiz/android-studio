76680669d761d16a0933df1c594699a3c804cefa
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-06 16:36:29 +0100

    perf: blocked landscape mode, changed layout to LinearLayout and edited some id and text views

--- CounterApp/app/src/main/java/roberherraiz/counter/MainActivity.java
+++ CounterApp/app/src/main/java/roberherraiz/counter/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends Activity {
 
     public void showCounter() {
 
-        TextView counterText = findViewById(R.id.counterTextView);
+        TextView counterText = findViewById(R.id.counterText);
         counterText.setText("" + count);
 
     }
