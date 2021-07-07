454906fdc7f2eb4459c95d84e7a85eb9bf781f0c
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-07 20:07:26 +0100

    feat: now the user can allow negatives and set a reset counter number

--- CounterApp/app/src/main/java/roberherraiz/counter/MainActivity.java
+++ CounterApp/app/src/main/java/roberherraiz/counter/MainActivity.java
@@ -3,43 +3,55 @@ package roberherraiz.counter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     public int count;
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        resultText = (TextView) findViewById(R.id.counterText);
         count = 0;
-        showCounter();
     }
 
     /* Las funciones de lógica de eventos deben recibir el View, osea, el botón en estos casos */
 
     public void increaseCounter(View view) {
         count++;
-        showCounter();
+        resultText.setText("" + count);
     }
 
     public void decreaseCounter(View view) {
         count--;
-        showCounter();
+        if (count < 0) {
+            CheckBox negatives = (CheckBox)findViewById(R.id.negativeCheckBox);
+            if (!negatives.isChecked()) {
+                count = 0;
+            }
+        }
+        resultText.setText("" + count);
     }
 
     public void resetCounter(View view) {
-        count = 0;
-        showCounter();
+        EditText resetNumber = (EditText)findViewById(R.id.nReset);
+
+        try {
+        count = Integer.parseInt(resetNumber.getText().toString());
+        } catch (Exception e) {
+            count = 0;
+        }
+
+        resetNumber.setText(""); // reseteamos el resetNumber
+        resultText.setText("" + count);
     }
 
-    public void showCounter() {
 
-        TextView counterText = findViewById(R.id.counterText);
-        counterText.setText("" + count);
-
-    }
 
 }
