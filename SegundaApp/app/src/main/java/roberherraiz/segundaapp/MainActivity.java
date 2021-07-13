5767c654100602811f8ec006cd4b857bddf0c7df
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-13 19:09:28 +0100

    feat: used java for setting up a android text on two locales

--- SegundaApp/app/src/main/java/roberherraiz/segundaapp/MainActivity.java
+++ SegundaApp/app/src/main/java/roberherraiz/segundaapp/MainActivity.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ImageView img1 = findViewById(R.id.editIcon);
+//
+//        img1.setImageResource(android.R.drawable.ic_menu_edit);
+
+        TextView text1 = findViewById(R.id.cancelText);
+        text1.setText(android.R.string.cancel);
+
 
     }
 }
