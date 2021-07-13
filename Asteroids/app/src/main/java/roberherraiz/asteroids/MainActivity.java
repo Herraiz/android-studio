1d29cb357d690b3741044891880565686d9ddd15
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-13 20:02:34 +0100

    feat: added a new 'about' activity with translations and added a meteor img to activity main

--- Asteroids/app/src/main/java/roberherraiz/asteroids/MainActivity.java
+++ Asteroids/app/src/main/java/roberherraiz/asteroids/MainActivity.java
@@ -2,7 +2,10 @@ package roberherraiz.asteroids;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /* Creamos un método con la intención que ejecutará nuestra nueva actividad */
+    public void run_about(View view) {
+        Intent i = new Intent(this, AboutActivity.class);
+        startActivity(i);
+
+    }
+
 }
