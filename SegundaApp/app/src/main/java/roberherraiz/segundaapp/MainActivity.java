03cb9d8e0109588dda6bf00ef334e9c234ad1766
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-13 17:36:51 +0100

    feat: learning how to use image resources

--- SegundaApp/app/src/main/java/roberherraiz/segundaapp/MainActivity.java
+++ SegundaApp/app/src/main/java/roberherraiz/segundaapp/MainActivity.java
@@ -3,6 +3,7 @@ package roberherraiz.segundaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img = findViewById(R.id.logoImage);
+
+        img.setImageResource(R.drawable.logo);
+
     }
 }
