2525a70f445f835667432497c5de43c8a123a3cd
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-19 17:36:28 +0100

    feat: added 2 layouts

--- DatosActividades/app/src/main/java/com/example/datosactividades/Sumar.java
+++ DatosActividades/app/src/main/java/com/example/datosactividades/Sumar.java
@@ -1,2 +1,13 @@
-package com.example.datosactividades;public class Sumar {
+package com.example.datosactividades;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Sumar extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
 }
