509e6ca3e05a25c94fdae66eced32635e443f6c7
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-19 17:52:39 +0100

    feat: added functionality to the Sumar layout

--- DatosActividades/app/src/main/java/com/example/datosactividades/MainActivity.java
+++ DatosActividades/app/src/main/java/com/example/datosactividades/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void aSumar(View view) {
+
+        EditText box1 = findViewById(R.id.num1);
+        int num1 = Integer.parseInt(box1.getText().toString());
+        EditText box2 = findViewById(R.id.num2);
+        int num2 = Integer.parseInt(box2.getText().toString());
+
         Intent i = new Intent(this, Sumar.class);
+
+        /* Contenido del bundle que enviamos al layout Sumar */
+        i.putExtra("numero1", num1);
+        i.putExtra("numero2", num2);
+
         startActivity(i);
     }
 
--- DatosActividades/app/src/main/java/com/example/datosactividades/Sumar.java
+++ DatosActividades/app/src/main/java/com/example/datosactividades/Sumar.java
@@ -2,6 +2,7 @@ package com.example.datosactividades;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Sumar extends Activity {
 
@@ -9,5 +10,15 @@ public class Sumar extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result);
+
+        /* Almacenamos los datos del Bundle */
+        Bundle data = getIntent().getExtras(); // esto se ejecuta sobre la activity
+
+        int num1 = data.getInt("numero1");
+        int num2 = data.getInt("numero2");
+        int result = num1 + num2;
+
+        TextView resultText = findViewById(R.id.textoResultado);
+        resultText.setText("El resultado es " + result);
     }
 }
