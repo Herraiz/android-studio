4e969270fcf6e0b90d5122b66b81a6dc7a208bcd
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-20 19:34:02 +0100

    feat: worked with Paths and TextOnPath

--- VistasGraficos/app/src/main/java/roberherraizcom/vistasgraficos/MainActivity.java
+++ VistasGraficos/app/src/main/java/roberherraizcom/vistasgraficos/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 
@@ -27,18 +29,36 @@ public class MainActivity extends AppCompatActivity {
 
         protected void onDraw(Canvas canvas) {
 
+           /* Forma de dibujar sin usar path
             Paint myBrush = new Paint();
             myBrush.setColor(Color.argb(185, 168, 155, 255));
             myBrush.setStrokeWidth(8);
             myBrush.setStyle(Paint.Style.STROKE);
             canvas.drawCircle(175, 175, 100, myBrush);
 
-
             int myColor;
             myColor = ContextCompat.getColor(getContext(), R.color.circleFill);
             myBrush.setColor(myColor);
             myBrush.setStyle(Paint.Style.FILL);
-            canvas.drawCircle(175, 175, 100, myBrush);
+            canvas.drawCircle(175, 175, 100, myBrush); */
+
+            /* Dibujamos un círculo */
+            Path myPath = new Path();
+            myPath.addCircle(350, 350, 200, Path.Direction.CCW);
+            Paint myBrush = new Paint();
+            myBrush.setColor(Color.RED);
+            myBrush.setStrokeWidth(8);
+            myBrush.setStyle(Paint.Style.STROKE);
+            canvas.drawPath(myPath, myBrush);
+
+            /* Escribimos en el círculo */
+            myBrush.setStrokeWidth(1);
+            myBrush.setStyle(Paint.Style.FILL);
+            myBrush.setTextSize(40);
+            myBrush.setTypeface(Typeface.SANS_SERIF);
+            canvas.drawTextOnPath("Curso Android Píldoras Informáticas", myPath,40, 100, myBrush);
+
+
 
         }
 
