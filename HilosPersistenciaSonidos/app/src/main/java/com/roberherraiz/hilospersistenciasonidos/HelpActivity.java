5c77654aa4b8e5b16e579267ffe016f87759d3a7
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-09-06 16:14:38 +0100

    feat: added the back button to the help activity and set up the default orientation

--- HilosPersistenciaSonidos/app/src/main/java/com/roberherraiz/hilospersistenciasonidos/HelpActivity.java
+++ HilosPersistenciaSonidos/app/src/main/java/com/roberherraiz/hilospersistenciasonidos/HelpActivity.java
@@ -3,6 +3,7 @@ package com.roberherraiz.hilospersistenciasonidos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+    }
+
+    public void volver(View view){
+
+        onBackPressed();
+
     }
 }
